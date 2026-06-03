1e96de05e5e0be863579a2323404b00ead5da74d
Renan <renandiutida@gmail.com>
2026-06-03 14:48:37 -0300

    Configuracao de appsettings.json e Program.cs base com registros comentados

--- CentimeterX.API/Program.cs
+++ CentimeterX.API/Program.cs
@@ -1,11 +1,27 @@
+//using CentimeterX.API.Data;
+//using CentimeterX.API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Oracle + EF Core
+//var connectionString = builder.Configuration.GetConnectionString("OracleConnection");
+//builder.Services.AddDbContext<AppDbContext>(options =>
+    //options.UseOracle(connectionString));
+
+// Services
+//builder.Services.AddScoped<RoverService>();
+//builder.Services.AddScoped<SessaoCorrecaoService>();
+//builder.Services.AddScoped<EstacaoBaseService>();
+//builder.Services.AddScoped<OcorrenciaService>();
+//builder.Services.AddScoped<UsuarioService>();
+//builder.Services.AddScoped<ICorrecaoService, SessaoCorrecaoService>();
 
 var app = builder.Build();
 
@@ -17,9 +33,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
